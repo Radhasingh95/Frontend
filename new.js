c9e81e0730f853852006f2b593d973dcfb524dd2
Radha <radhasingh@usthaan.in>
2025-12-23 12:04:54 +0530

    JS intro

---
+++ new.js
@@ -0,0 +1,3 @@
+// Running js in terminal using node new.js 
+console.log("hello comment here");
+console.log("code is running");
---
+++ script.js
@@ -0,0 +1,17 @@
+alert("Hello world");
+var a=prompt("enter your number");
+console.log("Your number is "+a);
+
+var isTrue = confirm("Are you sure you want to leave this page and blast your computer ")
+
+if(isTrue){
+    console.log("Computer is blasting")
+}
+
+else{
+    console.log("Computer is not blasting")
+
+}
+document.title="I m good";
+// inline css 
+document.body.style.backgroundColor="red";
